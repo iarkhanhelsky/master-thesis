e6cdb96ff82af97a83101a2319413af3fa37ad77
Ilya Arkhanhelsky <dm@dm>
2015-02-05 11:39:49 +0300

    independend build.r

--- build.r
+++ build.r
@@ -1,3 +1,11 @@
-setwd('~/Projects/uni/diploma/src')
+getFilename <- function() {
+   args <- commandArgs()
+   filearg <- grep("^--file=", args, value=TRUE)
+   if (length(filearg))
+     sub("^--file=", "", filearg)
+   else
+     invisible(NULL)
+}
+setwd(paste(dirname(normalizePath(getFilename())),'/src', sep=""))
 require('rmarkdown')
 rmarkdown::render('diploma.Rmd', 'pdf_document', output_dir='../out')
