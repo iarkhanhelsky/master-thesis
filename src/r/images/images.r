f96ef01fe9ce895d137d00ac58d00154c6899058
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2015-03-30 14:25:06 +0300

    require hash

--- src/r/images/images.r
+++ src/r/images/images.r
@@ -1,10 +1,12 @@
 require('png')
 require('tools')
+require('hash')
+
 image.read <- function(src) {
 	read.routes <- hash()
 	read.routes[['png']] <- readPNG
 	ext <- tolower(file_ext(src))
-	
+
 	stopifnot(!is.null(read.routes[[ext]]))
 
 	return(read.routes[[ext]](src))
@@ -16,12 +18,9 @@ image.bytes <- function(floats, color.quant = 255) {
 }
 
 image.bits <- function(data, keep = 8) {
-	apply(data, 1:length(dim(data)), integer.bits, keep = keep)
+	apply(data, 1, integer.bits, keep = keep)
 }
 
 integer.bits <- function(x, keep = 8) {
-	len <- as.integer(log2(x)) + 1
-	shift <- len - keep
-	bits <- intToBits(x)[(max(shift, 0) + 1):len]
-	c(replicate(ifelse(shift < 0, -shift, 0), 0), rev(as.integer(bits)))
+	intToBits(x)
 }
