65dfeb9e773739ad5ddd7b03cb4e11f3b443b78c
iarkhanhelsky <ilya.arkhanhelsky@gmail.com>
2015-05-22 13:57:37 +0300

    Denoising theory finished

---
+++ build_appendix.r
@@ -0,0 +1,11 @@
+getFilename <- function() {
+  args <- commandArgs()
+  filearg <- grep("^--file=", args, value=TRUE)
+  if (length(filearg))
+    sub("^--file=", "", filearg)
+  else
+    invisible(NULL)
+}
+setwd(paste(dirname(normalizePath(getFilename())),'/src', sep=""))
+require('rmarkdown')
+rmarkdown::render('appendix.Rmd', 'pdf_document', output_dir='../out')
