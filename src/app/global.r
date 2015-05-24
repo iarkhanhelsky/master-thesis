024b972d45b513df50b3b0b6dd484eee84966d33
iarkhanhelsky <ilya.arkhanhelsky@gmail.com>
2015-05-24 12:19:15 +0300

    Added real visualisation to thresholds

--- src/app/global.r
+++ src/app/global.r
@@ -2,3 +2,5 @@ source('modules/denoise.r')
 source('modules/thresholds.r')
 
 source('../r/helpers/visual.r')
+
+source('../r/denoising.r')
--- src/app/modules/thresholds.r
+++ src/app/modules/thresholds.r
@@ -1,29 +1,30 @@
+lambda.range.max <- 10
 
 ui.thresholds <- function() {
-  lambda.range.max <- 10
-  
   tabPanel("Расчет и визуализация пороговых значений",
-         mainPanel(
-           h3("Визуализация жесткой и мягкой замены значений вейвлет коэффициентов"),
-           sliderInput("lambda", "Lambda", 
-                       min = 0, max = lambda.range.max, value = 2, step= 0.5),
-           plotOutput("thresholding"),
-           h3("Расчет универсального порогового значения в зависимости от размера выборки"),
-           sliderInput("sample.length", "n",
-                       min = 100, max = 10000, value = 1000, step = 100),
-           plotOutput("unviversal")))
+           withMathJax(
+             mainPanel(
+               h3("Визуализация жесткой и мягкой замены значений вейвлет коэффициентов"),
+               sliderInput("lambda", "$$\\lamda$$",
+                           min = 0, max = lambda.range.max, value = 2, step= 0.5),
+               plotOutput("thresholding"),
+               h3("Расчет универсального порогового значения в зависимости от размера выборки"),
+               sliderInput("sample.length", "n",
+                           min = 100, max = 10000, value = 1000, step = 100),
+               plotOutput("unviversal"))))
 }
 
 srv.thresholds <- function(input, output) {
   output$thresholding <- renderPlot({
     lambda <- input$lambda
+    t <- seq(-lambda.range.max, lambda.range.max, 0.01)
     visual.multiplot(
-      qplot(seq(0, lambda*pi, 0.05), sin(seq(0, lambda*pi, 0.05)), geom='line', main='Жесткая замена'),
-      qplot(seq(0, lambda*pi, 0.05), cos(seq(0, lambda*pi, 0.05)), geom='line', main='Mягкая замена'),      
+      qplot(t, thresholding.hard(t, lambda), geom='line', main='Жесткая замена', xlab='t', ylab=expression(delta[h])),
+      qplot(t, thresholding.soft(t, lambda), geom='line', main='Mягкая замена', xlab='t', ylab=expression(delta[s])),
       cols = 2)})
-  
+
   output$universal <- renderPlot({
     bound <- input$sample.length
-    
+
   })
 }
